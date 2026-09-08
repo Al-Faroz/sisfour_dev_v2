f08002b535c445f67f0e460e0286572664de4adb
Al-Faroz <151546815+Al-Faroz@users.noreply.github.com>
2026-09-08 17:14:07 +0700

    Fix vendor script paths v0.5

--- app/Views/_scripts.php
+++ app/Views/_scripts.php
@@ -4,9 +4,9 @@
 <script src="<?= base_url('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/js/menu.js') ?>"></script>
 
-<script src="<?= base_url('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') ?>"></script>
-<script src="<?= base_url('assets/vendor/libs/select2/select2.js') ?>"></script>
-<script src="<?= base_url('assets/vendor/libs/sweetalert2/sweetalert2.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/libs/datatables/datatables.min.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/libs/select2/select2.min.js') ?>"></script>
+<script src="<?= base_url('assets/vendor/libs/sweetalert2/sweetalert2.all.min.js') ?>"></script>
 <script src="<?= base_url('assets/vendor/libs/apex-charts/apexcharts.js') ?>"></script>
 
 <!--
