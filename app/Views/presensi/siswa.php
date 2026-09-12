120a5452bd3b50cd5f04b7bc6c3536a7fac9b0d9
Al-Faroz <151546815+Al-Faroz@users.noreply.github.com>
2026-09-12 20:01:07 +0700

    G1: expose preloaded attendance context to direct dashboard flow

--- app/Views/presensi/siswa.php
+++ app/Views/presensi/siswa.php
@@ -110,4 +110,16 @@
     </div>
 </div>
 
+<?php if (isset($initialResult) && is_array($initialResult)): ?>
+<script type="application/json" id="presensiInitialResult"><?= json_encode(
+    $initialResult,
+    JSON_UNESCAPED_UNICODE
+    | JSON_UNESCAPED_SLASHES
+    | JSON_HEX_TAG
+    | JSON_HEX_AMP
+    | JSON_HEX_APOS
+    | JSON_HEX_QUOT
+) ?></script>
+<?php endif; ?>
+
 <?= $this->endSection() ?>
