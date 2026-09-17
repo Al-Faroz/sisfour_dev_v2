8a703fd48fe52de57d739ce31666f3831c9405c1
Al-Faroz <151546815+Al-Faroz@users.noreply.github.com>
2026-09-17 10:48:17 +0700

    feat(bk): add counseling follow-up routes

--- app/Config/RoutesBKFoundation.php
+++ app/Config/RoutesBKFoundation.php
@@ -15,6 +15,8 @@ $routes->group('', ['filter' => 'auth'], static function ($routes) {
         $routes->get('konseling/detail/(:num)', 'BKKonseling::detail/$1', ['filter' => 'permission:bk_konseling.view']);
         $routes->post('konseling/create', 'BKKonseling::create', ['filter' => 'permission:bk_konseling.manage']);
         $routes->put('konseling/update/(:num)', 'BKKonseling::update/$1', ['filter' => 'permission:bk_konseling.manage']);
+        $routes->post('konseling/(:num)/tindak-lanjut', 'BKKonseling::createFollowUp/$1', ['filter' => 'permission:bk_konseling.manage']);
+        $routes->put('konseling/tindak-lanjut/(:num)', 'BKKonseling::updateFollowUp/$1', ['filter' => 'permission:bk_konseling.manage']);
         $routes->get('konseling/export', 'BKKonseling::export', ['filter' => 'permission:bk_konseling.export']);
     });
 });
