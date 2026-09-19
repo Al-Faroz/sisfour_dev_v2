afc43def6b5c7fd99381d1a184a302a575e113eb
Al-Faroz <151546815+Al-Faroz@users.noreply.github.com>
2026-09-19 08:55:52 +0700

    feat(ptsp): submit auto-download PDF setting

--- assets/js/settings/sistem.js
+++ assets/js/settings/sistem.js
@@ -60,6 +60,7 @@ formSistem?.addEventListener('submit', async event => {
     const payload = Object.fromEntries(fd.entries());
     payload.geofencing_aktif = form.elements.geofencing_aktif.checked ? '1' : '0';
     payload.ptsp_layanan_auto_print = form.elements.ptsp_layanan_auto_print?.checked ? '1' : '0';
+    payload.ptsp_layanan_auto_download_pdf = form.elements.ptsp_layanan_auto_download_pdf?.checked ? '1' : '0';
 
     const response = await fetch(`${base}/settings/sistem/update`, {
       method: 'PUT',
